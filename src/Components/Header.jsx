7ce50ed37ed8c082b91668c78cee917b12c1fc71
Naji <lemiine987@gmail.com>
2023-04-15 11:42:39 +0300

    Create the header section

---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center">
+      {/* The right side of the Header */}
+
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsInfoCircleFill} />
+      </div>
+
+      {/* The left side of the Header */}
+
+      <div className="">
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+}
